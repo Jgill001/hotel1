60bd562647b9263f1d30c0f9965d30732f9e73a7
Jgill001 <123706312+Jgill001@users.noreply.github.com>
2026-04-13 13:59:34 +1000

    Update poki-sdk.js

--- poki-sdk.js
+++ poki-sdk.js
@@ -1,11 +1,22 @@
 window.PokiSDK = {
     init: () => Promise.resolve(),
     commercialBreak: () => Promise.resolve(false),
-    rewardedBreak: () => Promise.resolve(true), // Instantly grant the reward
+    
+    // We add a 1-second delay so the game doesn't think we are cheating
+    rewardedBreak: () => new Promise(resolve => {
+        setTimeout(() => {
+            resolve(true);
+        }, 1000);
+    }),
+    
     setDebug: () => {},
     gameplayStart: () => {},
     gameplayStop: () => {},
     gameLoadingFinished: () => {},
     gameInteractive: () => {}
 };
+
+// THE MISSING PIECES (What caused your popup error):
+window.rewardedBreak = window.PokiSDK.rewardedBreak;
+window.commercialBreak = window.PokiSDK.commercialBreak;
 window.initPokiBridge = () => true;
